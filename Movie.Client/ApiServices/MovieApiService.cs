4f3a3caecc32a7b685e71bd31fe9ea0e176c463f
14024212 <14024212@161-1721>
2024-03-02 14:04:37 +0330

    Movie Client

---
+++ Movie.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,19 @@
+﻿namespace Movie.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<List<Models.Movie>> GetAll()
+        {
+            return Task.FromResult(new List<Models.Movie>() {
+             new Models.Movie()
+             {
+                  CreatedAt = DateTime.Now,
+                  Genre = "Sienc Fiction",
+                  Id = 1,
+                  Name = "Test",
+                  Owner ="Javad Bayat"
+             }
+            });
+        }
+    }
+}
---
+++ Movie.Client/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie.Client.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Owner { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
